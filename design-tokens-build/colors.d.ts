8513b054c26f6e6cf3d5533d1e40d7d2a896a9fa
Marianne Røsvik <marianne.rosvik@digdir.no>
2026-01-30 08:34:14 +0100

    new theme colors

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,23 @@
+/* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
+/* build: v1.11.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    accent: never;
+    ekstra1: never;
+    ekstra2: never;
+    ekstra3: never;
+    brand1: never;
+    brand2: never;
+    brand3: never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
---
+++ design-tokens-build/types.d.ts
@@ -0,0 +1,22 @@
+/* build: v1.11.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    accent: never;
+    ekstra1: never;
+    ekstra2: never;
+    ekstra3: never;
+    brand1: never;
+    brand2: never;
+    brand3: never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
